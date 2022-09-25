209917b4b662cf036a08d045e5a59c6ab009679f
AnastasiiaSteblyna <zanegina93@gmail.com>
2022-09-25 11:35:15 +0300

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = galleryItems;
+const galleryBox = document.querySelector(".gallery");
+const galleryImages = createGallery();
+galleryBox.insertAdjacentHTML("beforeend", galleryImages);
+
+function createGallery() {
+  return gallery
+    .map(
+      (img) => `<div class="gallery__item">
+  <a class="gallery__link" href=${img.original}>
+    <img
+      class="gallery__image"
+      src=${img.preview}
+      data-source=${img.original}
+      alt=${img.description}
+    />
+  </a>
+</div>`
+    )
+    .join("");
+}
+
+galleryBox.addEventListener(`click`, (e) => {
+  e.preventDefault();
+
+  const instance = basicLightbox.create(`
+    <img src= "${e.target.dataset.source}">
+`);
+
+  instance.show();
+
+  window.addEventListener(`keydown`, onEscapeClose);
+
+  function onEscapeClose(e) {
+    if (e.code !== "Escape") return;
+    instance.close();
+    window.removeEventListener(`keydown`, onEscapeClose);
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = galleryItems;
+const galleryBox = document.querySelector(".gallery");
+const galleryImages = createGallery();
+galleryBox.insertAdjacentHTML("beforeend", galleryImages);
+
+function createGallery() {
+  return gallery
+    .map(
+      (img) => `<a class="gallery__item" href=${img.original}>
+  <img class="gallery__image" src=${img.preview} alt=${img.description} title=${img.description}/>
+</a>`
+    )
+    .join("");
+}
+
+galleryBox.addEventListener(`click`, (e) => {
+  e.preventDefault();
+});
+
+new SimpleLightbox(`.gallery a`, { captionDelay: 250 });
